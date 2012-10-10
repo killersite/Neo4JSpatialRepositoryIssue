34039323e87fb0284ffed17b20a629fb3eb57bbc
Ben Schochet <ben@killersite.com>
2012-10-10 07:29:56 -0400

    initial push

---
+++ src/main/java/com/ex/neorepository/RestaurantNeoRepository.java
@@ -0,0 +1,10 @@
+package com.ex.neorepository;
+
+import org.springframework.data.neo4j.repository.GraphRepository;
+import org.springframework.data.neo4j.repository.SpatialRepository;
+
+import com.ex.domain.Restaurant;
+
+public interface RestaurantNeoRepository extends GraphRepository<Restaurant>, SpatialRepository<Restaurant> {
+
+}
